2f47add2f1c7c0db6d4b6be563d0316253e88ff2
Siroshun09 <siro.shun8@gmail.com>
2024-08-16 04:13:44 +0900

    Ignore VehicleExitEvent for prevent stuck on animal

--- worldguard-bukkit/src/main/java/com/sk89q/worldguard/bukkit/listener/RegionProtectionListener.java
+++ worldguard-bukkit/src/main/java/com/sk89q/worldguard/bukkit/listener/RegionProtectionListener.java
@@ -591,7 +591,7 @@ public class RegionProtectionListener extends AbstractListener {
         }
     }
 
-    @EventHandler(ignoreCancelled = true)
+    // @EventHandler(ignoreCancelled = true)
     public void onVehicleExit(VehicleExitEvent event) {
         Entity vehicle = event.getVehicle();
         if (!isRegionSupportEnabled(vehicle.getWorld())) return; // Region support disabled
